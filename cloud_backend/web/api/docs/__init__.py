eda40c01a0e91cc85e436027a6aae91f5751857d
citylameR <davidtscher@gmail.com>
2024-05-04 15:58:19 +0300

    Initial commit

---
+++ cloud_backend/web/api/docs/__init__.py
@@ -0,0 +1,4 @@
+"""Routes for swagger and redoc."""
+from cloud_backend.web.api.docs.views import router
+
+__all__ = ["router"]
---
+++ cloud_backend/web/application.py
@@ -0,0 +1,68 @@
+import logging
+from importlib import metadata
+from pathlib import Path
+
+import sentry_sdk
+from fastapi import FastAPI
+from fastapi.responses import UJSONResponse
+from fastapi.staticfiles import StaticFiles
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+from cloud_backend.logging import configure_logging
+from cloud_backend.settings import settings
+from cloud_backend.web.api.router import api_router
+from cloud_backend.web.lifetime import register_shutdown_event, register_startup_event
+
+APP_ROOT = Path(__file__).parent.parent
+
+
+def get_app() -> FastAPI:
+    """
+    Get FastAPI application.
+
+    This is the main constructor of an application.
+
+    :return: application.
+    """
+    configure_logging()
+    if settings.sentry_dsn:
+        # Enables sentry integration.
+        sentry_sdk.init(
+            dsn=settings.sentry_dsn,
+            traces_sample_rate=settings.sentry_sample_rate,
+            environment=settings.environment,
+            integrations=[
+                FastApiIntegration(transaction_style="endpoint"),
+                LoggingIntegration(
+                    level=logging.getLevelName(
+                        settings.log_level.value,
+                    ),
+                    event_level=logging.ERROR,
+                ),
+            ],
+        )
+    app = FastAPI(
+        title="cloud_backend",
+        version=metadata.version("cloud_backend"),
+        docs_url=None,
+        redoc_url=None,
+        openapi_url="/api/openapi.json",
+        default_response_class=UJSONResponse,
+    )
+
+    # Adds startup and shutdown events.
+    register_startup_event(app)
+    register_shutdown_event(app)
+
+    # Main router for the API.
+    app.include_router(router=api_router, prefix="/api")
+    # Adds static directory.
+    # This directory is used to access swagger files.
+    app.mount(
+        "/static",
+        StaticFiles(directory=APP_ROOT / "static"),
+        name="static",
+    )
+
+    return app
